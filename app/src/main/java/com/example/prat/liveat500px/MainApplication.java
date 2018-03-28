06d3792e28a1592a620cf19ebc3e994fa72d3803
Prat <prat.s@iconext.co.th>
2018-03-28 11:53:51 +0700

    add thecheeselibrary

--- app/src/main/java/com/example/prat/liveat500px/MainApplication.java
+++ app/src/main/java/com/example/prat/liveat500px/MainApplication.java
@@ -2,6 +2,8 @@ package com.example.prat.liveat500px;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by Prat on 3/28/2018.
  */
@@ -13,6 +15,7 @@ public class MainApplication extends Application {
         super.onCreate();
 
         //Initialize thing(s) here
+        Contextor.getInstance().init(getApplicationContext());
     }
 
     @Override
