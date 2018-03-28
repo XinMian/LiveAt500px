70548ce5890efaec61620f802103aada74cfb2e1
Prat <prat.s@iconext.co.th>
2018-03-28 11:10:57 +0700

    start project

---
+++ app/src/main/java/com/example/prat/liveat500px/MainApplication.java
@@ -0,0 +1,22 @@
+package com.example.prat.liveat500px;
+
+import android.app.Application;
+
+/**
+ * Created by Prat on 3/28/2018.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Initialize thing(s) here
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
---
+++ app/src/main/java/com/example/prat/liveat500px/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.prat.liveat500px.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.prat.liveat500px.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
